c9d2ec56491e0509e4dfdf550f6c012df8062787
vagrant <vagrant@rental>
2018-10-10 12:03:00 +0000

    sort v1.01

--- frontend/modules/rent/views/construction/view.php
+++ frontend/modules/rent/views/construction/view.php
@@ -4,17 +4,33 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 
 ?>
-<?php foreach ($item as $items): ?>
-    <div>
-        <a href="<?php echo Url::to(['/rent/construction/equipment', 'id' => $items->id]); ?>">
-            <p><?= Html::encode($items->name) ?></p>
-        </a>
-        <div class="col-md-12">
-            <a href="<?php echo Url::to(['/rent/construction/equipment', 'id' => $items->id]); ?>">
-                <img src="<?php echo Yii::$app->storage->getFile($items->filename); ?>"
-            </a>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-8" style="background: yellow">
+
+            <div class="row" style="background: red">
+                <?php foreach ($item as $items): ?>
+                    <div class="col-lg-8" style="background: brown">
+                        <a href="<?php echo Url::to(['/rent/construction/equipment', 'id' => $items->id]); ?>">
+                            <img src="<?php echo Yii::$app->storage->getFile($items->filename); ?> "
+                                 class="img-thumbnail" alt="Cinque Terre">
+                        </a>
+                    </div>
+                    <div class="col-lg-4" style="background: palegreen">
+                        <a href="<?php echo Url::to(['/rent/construction/equipment', 'id' => $items->id]); ?>">
+                            <p><?= Html::encode($items->name); ?></p>
+                        </a>
+                        <p>etrdu tifuyglihj; etrduti fuyglihj; etrdutifuy glihj;etrdut ifuyglihj;etrdu tifuyglihj; etrdutifuyglihj;etrduti uyglihj; etrdutif uyglihj;</p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+sdfsd
         </div>
 
 
+        <div class="col-lg-4" style="background: #00CC00;">
+
+        </div>
     </div>
-<?php endforeach; ?>
+</div>
